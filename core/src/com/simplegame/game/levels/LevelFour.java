0a19e7bc5f564ff7942fa174a10c1331c7218deb
ravisharma1987 <ravisharma1987@gmail.com>
2016-10-24 19:27:08 +0200

    added ios-moe

--- core/src/com/simplegame/game/levels/LevelFour.java
+++ core/src/com/simplegame/game/levels/LevelFour.java
@@ -1,55 +1,19 @@
 package com.simplegame.game.levels;
 
-
-import com.simplegame.game.screens.GameScreen;
+import com.uwsoft.editor.renderer.Overlap2DStage;
 
 /**
  * Created by ravi on 23.10.16.
  */
 
-public class LevelFour extends GameScreen {
-    @Override
-    protected void setScreenResolution() {
+public class LevelFour extends Overlap2DStage {
 
-    }
+    public LevelFour() {
+        initSceneLoader();
 
-    @Override
-    protected void setCamera() {
+        sceneLoader.loadScene("MainScene");
 
-    }
-
-    @Override
-    public void show() {
-
-    }
-
-    @Override
-    public void render(float delta) {
-
-    }
-
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-
+        // Get the root actor and add it to stage
+        addActor(sceneLoader.getRoot());
     }
 }
