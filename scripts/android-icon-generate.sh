c61eb7c26ce23216823769c3e1b873fbd5b7ea5b
Ravi Sharma <ravisharma1987@gmail.com>
2017-07-19 22:17:33 +0200

    Release 4.0

---
+++ scripts/android-icon-generate.sh
@@ -0,0 +1,10 @@
+mkdir drawable-mdpi
+convert icon.png -resize 48x48  drawable-mdpi/ic_launcher.png
+mkdir drawable-hdpi
+convert icon.png -resize 72x72  drawable-hdpi/ic_launcher.png
+mkdir drawable-xhdpi
+convert icon.png -resize 96x96  drawable-xhdpi/ic_launcher.png
+mkdir drawable-xxhdpi
+convert icon.png -resize 144x144  drawable-xxhdpi/ic_launcher.png
+mkdir drawable-xxxhdpi
+convert icon.png -resize 192x192  drawable-xxxhdpi/ic_launcher.png
